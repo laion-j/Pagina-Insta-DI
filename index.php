f107910be7095be8bc7943fbf6e225fe934ff90c
Laion <laionjordy07@hotmail.com>
2020-10-24 14:04:50 -0300

    HTML Estruturado

--- index.php
+++ index.php
@@ -4,14 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intagram</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="insta-wrapper">
         <div class="insta-phone">
-            <img src=".src/phone.png >
         </div>
         <div class="insta-continue">
-        
+            <div class="group">
+                <img src="/src/insta-logo.png" class="insta-logo" alt="instagam logo">
+                <div class="profile-photo">
+                    <img src="./src/neymar.jpg" alt="foto de perfil">
+                </div>
+                <a href="#" class="insta-login">Continue como jrNeymar</a>
+                <a href="#" class="insta-logout">Remover conta</a>
+            </div>
+            <div class="group">
+                <p class="not-account">Não é jrNeymar?</p>
+                <p class="notaccount">
+                    <span class="linkblue">Alternar contas</span>
+                    ou
+                    <span class="linkblue">Inscreva-se</span>
+                </p>
+            </div>
+            <div class="get-the-app">
+                <p class="get-app">Baixe o aplicativo</p>
+                <div class="download">
+                    <a href="#" class="app-download"></a>
+                    <a href="#" class="app-download"></a>
+                </div>
+            </div>
         </div>
     </div>
 </body>
