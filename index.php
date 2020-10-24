7103b1b0d4cb210ac95f0864a2e34744aeb92a18
Laion <laionjordy07@hotmail.com>
2020-10-24 12:46:32 -0300

    Adicionando imagens e index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intagram</title>
+</head>
+<body>
+    <div class="insta-wrapper">
+        <div class="insta-phone">
+            <img src=".src/phone.png >
+        </div>
+        <div class="insta-continue">
+        
+        </div>
+    </div>
+</body>
+</html>
